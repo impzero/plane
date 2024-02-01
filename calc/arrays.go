9f581b42ea3b16156dff9ee8c5e013ccf5d33101
Lyubo Lyubchev <lyubo_2317@abv.bg>
2024-02-01 15:17:21 +0100

    add getCenterOffset function to calculate center offset in arrays

---
+++ calc/arrays.go
@@ -0,0 +1,22 @@
+package calc
+
+import "math"
+
+func getCenterOffset(size int, point int) int {
+	if point > size {
+		return -1
+	}
+	if size%2 == 0 {
+		leftCenterIndex := size/2 - 1
+		rightCenterIndex := size / 2
+
+		if point <= leftCenterIndex {
+			return leftCenterIndex - point
+		} else {
+			return point - rightCenterIndex
+		}
+	}
+
+	centerIndex := size / 2
+	return int(math.Abs(float64(centerIndex - point)))
+}
---
+++ calc/arrays_test.go
@@ -0,0 +1,30 @@
+package calc
+
+import "testing"
+
+func Test_getCenterOffset(t *testing.T) {
+	type args struct {
+		size int
+		i    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "size 6 i 1", args: args{size: 6, i: 1}, want: 1},
+		{name: "size 6 i 3", args: args{size: 6, i: 3}, want: 0},
+		{name: "size 6 i 5", args: args{size: 6, i: 5}, want: 2},
+		{name: "size 4 i 3", args: args{size: 5, i: 4}, want: 2},
+		{name: "size 3 i 0", args: args{size: 3, i: 0}, want: 1},
+		{name: "size 3 i 1", args: args{size: 3, i: 1}, want: 0},
+		{name: "size 3 i 2", args: args{size: 3, i: 2}, want: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getCenterOffset(tt.args.size, tt.args.i); got != tt.want {
+				t.Errorf("getCenterOffset() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
