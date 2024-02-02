2fec5dfbe0f9c1c3444a1c39d3a657eab8dc3c72
Lyubo Lyubchev <lyubo_2317@abv.bg>
2024-02-02 18:38:29 +0100

    refactor GetCenterOffset function to use more descriptive variable names

--- calc/arrays.go
+++ calc/arrays.go
@@ -2,21 +2,21 @@ package calc
 
 import "math"
 
-func GetCenterOffset(size int, point int) int {
-	if point > size {
+func GetCenterOffset(arraySize int, arrayPoint int) int {
+	if arrayPoint > arraySize {
 		return -1
 	}
-	if size%2 == 0 {
-		leftCenterIndex := size/2 - 1
-		rightCenterIndex := size / 2
+	if arraySize%2 == 0 {
+		leftCenterIndex := arraySize/2 - 1
+		rightCenterIndex := arraySize / 2
 
-		if point <= leftCenterIndex {
-			return leftCenterIndex - point
+		if arrayPoint <= leftCenterIndex {
+			return leftCenterIndex - arrayPoint
 		} else {
-			return point - rightCenterIndex
+			return arrayPoint - rightCenterIndex
 		}
 	}
 
-	centerIndex := size / 2
-	return int(math.Abs(float64(centerIndex - point)))
+	centerIndex := arraySize / 2
+	return int(math.Abs(float64(centerIndex - arrayPoint)))
 }
